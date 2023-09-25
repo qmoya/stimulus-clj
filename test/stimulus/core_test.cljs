95242c91352f505d428a16ac738e9fcc76d22330
Quico Moya <me@qmoya.com>
2023-09-26 00:06:59 +0200

    Fix test

--- test/stimulus/core_test.cljs
+++ test/stimulus/core_test.cljs
@@ -1,14 +1,12 @@
 (ns stimulus.core-test
   (:require [cljs.test :refer [deftest is]]
-            [stimulus.controller :refer [->controller external-target-fn]]
-            [cljs.pprint :refer [pprint]]))
+            [stimulus.controller :refer [->controller]]))
 
 (deftest test-greet-controller
   (let [expansion (macroexpand '(->controller GreetController {:extends Controller
                                                                :targets ["output" "name"]
                                                                :actions {:greet (fn [this event]
                                                                                   (js/console.log event))}}))]
-    (pprint expansion)
     (is (= expansion '(do
                         (shadow.cljs.modern/defclass GreetController
                           (extends Controller)
@@ -23,5 +21,7 @@
                                              (.find targets "name")))
                           (greet [this event] ((fn [this event] (js/console.log event)) this event)))
                         (set! (.-targets GreetController) (clj->js ["output" "name"]))
+                        (defn get-output-target [controller] (.get-output-target controller))
+                        (defn get-name-target [controller] (.get-name-target controller))
                         GreetController)))))
 
